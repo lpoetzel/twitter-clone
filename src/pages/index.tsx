c4c113d971d1dae83d12ca2ac563b94a3a430648
Lars Poetzel <lars.poetzel@gmail.com>
2023-05-30 09:37:51 +0200

    following tweets logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ const Home: NextPage = () => {
         )}
       </header>
       <NewTweetForm />
-      <RecentTweets />
+      {selectedTab === "Recent" ? <RecentTweets /> : <FollowingTweets />}
     </>
   );
 };
@@ -58,4 +58,20 @@ export function RecentTweets() {
   );
 }
 
+export function FollowingTweets() {
+  const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
+    { onlyFollowing: true },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage}
+      fetchNewTweets={tweets.fetchNextPage}
+    />
+  );
+}
 export default Home;
