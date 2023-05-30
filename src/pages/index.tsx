946173a341efcf0d622b52f5c3577321870792af
Lars Poetzel <lars.poetzel@gmail.com>
2023-05-30 09:19:57 +0200

    tabs style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,38 @@ import { type NextPage } from "next";
 import { NewTweetForm } from "~/components/NewTweetForm";
 import { InfiniteTweetList } from "~/components/InfiniteTweetList";
 import { api } from "~/utils/api";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+
+const TABS = ["Recent", "Following"] as const;
 
 const Home: NextPage = () => {
+  const [selectedTab, setSelectedTab] =
+    useState<(typeof TABS)[number]>("Recent");
+  const session = useSession();
   return (
     <>
       <header className="sticky top-0 z-10 border-b bg-white pt-2">
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+        {session.status === "authenticated" && (
+          <div className="flex">
+            {TABS.map((tab) => {
+              return (
+                <button
+                  key={tab}
+                  className={`flex-grow p-2 hover:bg-gray-200 focus-visible:bg-gray-200 ${
+                    tab === selectedTab
+                      ? "border-b-4 border-b-blue-500 font-bold"
+                      : ""
+                  }`}
+                  onClick={() => setSelectedTab(tab)}
+                >
+                  {tab}
+                </button>
+              );
+            })}
+          </div>
+        )}
       </header>
       <NewTweetForm />
       <RecentTweets />
