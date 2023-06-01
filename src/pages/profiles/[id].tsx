5e554303ee64f482192710ca6fea71de40225be2
Lars Poetzel <lars.poetzel@gmail.com>
2023-06-01 08:49:13 +0200

    profile page logic

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -1,21 +1,40 @@
-import { NextPage } from "next";
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+  NextPage,
+} from "next";
 import Head from "next/head";
 import { ssgHelper } from "~/server/api/ssgHelper";
+import { api } from "~/utils/api";
+import ErrorPage from "next/error";
 
-const ProfilePage: NextPage = () => {
+const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  id,
+}) => {
+  const { data: profile } = api.profile.getById.useQuery({ id });
+  if (profile == null || profile.name == null) {
+    return <ErrorPage statusCode={404} />;
+  }
   return (
     <>
       <Head>
-        <title>{`Twitter Clone ${user.name}`}</title>
+        <title>{`Twitter Clone - ${profile.name}`}</title>
       </Head>
+      {profile.name}
     </>
   );
 };
-
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
 export async function getStaticProps(
-  context: GetStaticPathsContext<{ id: string }>
+  context: GetStaticPropsContext<{ id: string }>
 ) {
-  const id = context.params.id;
+  const id = context.params?.id;
 
   if (id == null) {
     return {
@@ -26,7 +45,14 @@ export async function getStaticProps(
   }
 
   const ssg = ssgHelper();
-  ssg.profile;
+  await ssg.profile.getById.prefetch({ id });
+
+  return {
+    props: {
+      id,
+      trpcState: ssg.dehydrate(),
+    },
+  };
 }
 
 export default ProfilePage;
