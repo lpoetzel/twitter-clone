c501f378c4b550c30074e3ec6b31136787a49c5b
Lars Poetzel <lars.poetzel@gmail.com>
2023-06-01 09:05:20 +0200

    profile tweetcount,followercount,followscount

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -8,6 +8,10 @@ import Head from "next/head";
 import { ssgHelper } from "~/server/api/ssgHelper";
 import { api } from "~/utils/api";
 import ErrorPage from "next/error";
+import Link from "next/link";
+import { IconHoverEffect } from "~/components/IconHoverEffect";
+import { VscArrowLeft } from "react-icons/vsc";
+import { ProfileImage } from "~/components/ProfileImage";
 
 const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   id,
@@ -21,10 +25,31 @@ const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
       <Head>
         <title>{`Twitter Clone - ${profile.name}`}</title>
       </Head>
-      {profile.name}
+      <header className="sticky top-0 z-10 flex items-center border-b bg-white px-4 py-2">
+        <Link href=".." className="mr-2">
+          <IconHoverEffect>
+            <VscArrowLeft className="h-6 w-6" />
+          </IconHoverEffect>
+        </Link>
+        <ProfileImage src={profile.image} className="flex-shrink-0" />
+        <div className="ml-2 flex-grow">
+          <h1 className="text-large font-bold">{profile.name}</h1>
+          <div className="text-gray-500 ">
+            {profile.tweetsCount}{" "}
+            {getPlural(profile.tweetsCount, "Tweet", "Tweets")} -{" "}
+            {getPlural(profile.followerCount, "Follower", "Followers")} -{" "}
+            {profile.followerCount} Following
+          </div>
+        </div>
+      </header>
     </>
   );
 };
+
+const PluralRules = new Intl.PluralRules();
+function getPlural(number: number, singular: string, plural: string) {
+  return PluralRules.select(number) === "one" ? singular : plural;
+}
 export const getStaticPaths: GetStaticPaths = () => {
   return {
     paths: [],
